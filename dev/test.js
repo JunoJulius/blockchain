d3b78e33660abe8c5235ea6f39931ec6b7f46e0b
Juno Julius <yunoyulius@gmail.com>
2018-09-29 20:53:18 +0200

    hash method tested and working

--- dev/test.js
+++ dev/test.js
@@ -1,18 +1,41 @@
 const Blockchain = require('./blockchain');
-
 const realcoin = new Blockchain();
-realcoin.createNewBlock(32421, 'SDFSDJFKKJ342FD', '23$KSJD324AKDO23');
 
 
-realcoin.createNewTransaction(100, 'JUNOKALD827397SD', 'JULIUSAKSJ9823');
+const previousBlockHash = 'DFKJSDf837498374SDFSDF'
+const currentBlockData = [
+{
+    amount: 10,
+    sender: 'ASDKJDF834734#2',
+    recipient: 'ASDKJ3472934#SDFS'
+},
+{
+    amount: 90,
+    sender: 'KOWIDJS#22212',
+    recipient: 'LDKFIOERU#ASDHY8787EHSDFS'
+},
+{
+    amount: 120,
+    sender: 'DFKSIERI#09982',
+    recipient: 'DOIEKJS#SDSDAL8734JSHFS'
+}
+];
+const nonce = 100;
 
-realcoin.createNewBlock(7659, 'SSDG675HJJEQ', 'KOYU887QXTYR');
 
-realcoin.createNewTransaction(100, 'JUNOKALD827397SD', 'JULIUSAKSJ9823');
-realcoin.createNewTransaction(10000, 'JUNOKALD827397SD', 'JULIUSAKSJ9823');
-realcoin.createNewTransaction(23800, 'JUNOKALD827397SD', 'JULIUSAKSJ9823');
-
-realcoin.createNewBlock(823, 'KSDLA9823JFDKF', 'EIRIEUR8373KAL');
+console.log(realcoin.hashBlock(previousBlockHash,currentBlockData, nonce));
+// realcoin.createNewBlock(32421, 'SDFSDJFKKJ342FD', '23$KSJD324AKDO23');
 
 
-console.log(realcoin);
+// realcoin.createNewTransaction(100, 'JUNOKALD827397SD', 'JULIUSAKSJ9823');
+
+// realcoin.createNewBlock(7659, 'SSDG675HJJEQ', 'KOYU887QXTYR');
+
+// realcoin.createNewTransaction(100, 'JUNOKALD827397SD', 'JULIUSAKSJ9823');
+// realcoin.createNewTransaction(10000, 'JUNOKALD827397SD', 'JULIUSAKSJ9823');
+// realcoin.createNewTransaction(23800, 'JUNOKALD827397SD', 'JULIUSAKSJ9823');
+
+// realcoin.createNewBlock(823, 'KSDLA9823JFDKF', 'EIRIEUR8373KAL');
+
+
+// console.log(realcoin);
