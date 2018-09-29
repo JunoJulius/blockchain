de3771803c9128e3497611dd4342a9c5ca9d3a58
Juno Julius <yunoyulius@gmail.com>
2018-09-30 00:05:36 +0200

    added genesis block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -47,9 +47,9 @@ Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData
     while (hash.substring(0, 4) !== '0000') {
         nonce++;
         hash = this.hashBlock(previousBlockHash, currentBlockData, nonce)
-    
+
         // console.log(hash); Uncomment this for some hash fun :)
-    
+
     }
     return nonce;
 }
