9e162b676f08723d12616e7bac35a3a6cd613540
Juno Julius <yunoyulius@gmail.com>
2018-09-30 00:51:16 +0200

    created /blockchain  GET endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 4242;
+const Blockchain = require('./blockchain');
+
+const realcoin = new Blockchain();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // fetch entire blockchain
-app.get('/blockchain', function (req, res){
- 
- 
+app.get('/blockchain', function (req, res) {
+res.send(realcoin);
 });
 
 // create a new transaction 
-app.post('/transaction', function (req, res){
+app.post('/transaction', function (req, res) {
     console.log(req.body);
     res.send(`the amount is ${req.body.amount} realcoin`)
 
 });
 
 //mine a new block 
-app.get('/mine', function (req, res){
+app.get('/mine', function (req, res) {
 
 });
 
